0d5f7a192ffd7907cd5390590c1263649853b64b
Vinicius1992-ms <vinicius20.ms@gmail.com>
2020-04-26 19:22:32 +0000

    few updates on the HTML file

--- routes.js
+++ routes.js
@@ -10,12 +10,3 @@ router.put('/items/:id', itemCtrl.updateItem);
 
 module.exports = router;
 
-// var router = express();
-
-// var express = require('express'),
-// router = express.Router();
-
-// router.use(express.static(path.resolve(__dirname, 'views')));
-// router.use(express.urlencoded({extended: true}));
-// router.use(express.json());
-// router.use(expAutoSan.all);
